70f4fb5e5053f3e7e9aa114fc6b94fdf1091cffd
Hamza :) <talkdrove@gmail.com>
2024-06-11 19:13:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "BY BYTE",
+  caption: process.env.CAPTION || "*Powered By HAMZA*",
   author: process.env.PACK_AUTHER || "TalkDrove",
   packname: process.env.PACK_NAME || "♥️",
   botname: process.env.BOT_NAME || "BYTE-MD",

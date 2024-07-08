ea09fde3f4414535529c9dfb76a970102b984ee2
zandat3 <133093258+zandat3@users.noreply.github.com>
2024-07-08 18:08:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
 global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.owner = process.env.OWNER_NUMBER || "923072380380"; 
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,7 +42,7 @@ global.scan = "https://byte-session.vercel.app/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicVBKWUtTVFgzOUEzTUc3STROS0pLZll2RG9YVjJ2c1FLNThtOEc2bEYzcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMXllSnk2Q2xwQ1paUDM0N0k2WWgvbVYyMWVYbjkza3ErVTg5cTRNTjFrYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI0Rks2YWd6REZLWUM3VkpXa3JnUFBqdGV1MkJpeWxpNHVSVk9sL3pCVDBvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJMaHh3cm0rT2N4cmJyRWtKTzVQaGQ2TG1RRW4yMytQSGtVUlFPQ3RKNkNBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9MOUxXNmVIVlpZZWpxSnBFdnFRR2d5WUJGUzNLYmN0YStGSXplS1dqRUk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilg5dy9LT3VoMW4yZkxYdnpmTWI5WDh4UCtMQll6VWV3WnpTZW5qT0c1RVU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ0Z5bVFna3czYWo1VGJOTk5NMEwrYWlOSG5VSzRvVUFCQ0RtdUErRlhHND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNjBTa1pJSXN4Mk9CL1c5S3ZlYjFKaEFZcHJybG0xQkpsRXUzSVNGeGpYOD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkhUWTAzVW9CcTNlcEZxVW9oSi9Pcno0MENsZnBsVnNtaHZ0UkFYVHpKeGFHcWxOckphQTRPUEdha0VSVTBCVzRMUUdlUHZrREcyMkNEQ016QzQ5NkJnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjE1LCJhZHZTZWNyZXRLZXkiOiJEUDQyWlBMK04vWmp1U0cwMTJUeThteSt2SGRLUDcwaVYwSnhsME1WMnEwPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJsZ3NQdWtmNlNpdWlMYlQ1dGIybEt3IiwicGhvbmVJZCI6ImRhYTVjZGU5LTFmZjYtNGE2MS04MzdjLWQzNjAzMDVlMDc1YSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCa1U3WGZKaWxJRGdKTWtQRkt2aGVPc0htTWM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVXhaMzVBY2lOTnhlYTdJaDFaUTlTa1NHMUtJPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlJTMlZaVkszIiwibWUiOnsiaWQiOiIyMzQ5MTYwMzc2NTUxOjEwQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNOZnozSjBGRU9qOHI3UUdHQVFnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJmUkxYaVBjclNHb0hjVzQ2c0ZmVnYyV3JzVHVoMFU5bThobXMzVC95OWk4PSIsImFjY291bnRTaWduYXR1cmUiOiJIemtxZDQzbzFrWVBKcmh1Z0xsNlNuS2M4N29FdkNwRllSMGRhSnpsOFZsN1lZYVJnTlhDQlRCbHRBRUo4MFBLZHZtdmdJejFBTlFESzg4dmVsNVdCdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiZisxdW9MeGQrTmNSYlNRM2ZjWWRWbWt0MUhDbVd6Q25LMHZkYSt1QnBuUjNocFoxd3ZldkVBMFhVei9ZQjNJcE1yYTlzbWp3bWVvMXRpMllNN0tEQ0E9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ5MTYwMzc2NTUxOjEwQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlgwUzE0ajNLMGhxQjNGdU9yQlgxYjlscTdFN29kRlBadklack4wLzh2WXYifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjA0NTA2NjYsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBR2h3In0=" //Make sure session id starts with Byte;;;
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ",",
@@ -53,7 +53,7 @@ module.exports = {
   packname: process.env.PACK_NAME || "♥️",
   botname: process.env.BOT_NAME || "BYTE-MD",
   ownername: process.env.OWNER_NAME || "Hamza",
-  errorChat: process.env.ERROR_CHAT || "923072380380",
+  errorChat: process.env.ERROR_CHAT || "923072380380", 
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
